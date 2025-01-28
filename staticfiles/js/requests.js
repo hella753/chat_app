2bcb114fcb5c2acc46d0ca6b48c5e4abf76c9282
Kristine Gaprindashvili <kristigaphrindashvili@gmail.com>
2025-01-28 13:36:45 +0400

    minor changes

---
+++ staticfiles/js/requests.js
@@ -0,0 +1,24 @@
+const friendRequestSocket = new WebSocket(
+    "ws://"
+    + window.location.host
+    + '/ws/friend_requests/'
+);
+
+
+friendRequestSocket.onclose = function (e) {
+    console.error('Friend Requests socket closed unexpectedly');
+};
+
+const friendRequestBtn = document.getElementById('friend-request-btn');
+const username = document.querySelector('.username-for-friend-requests')
+if (friendRequestBtn && username) {
+    friendRequestBtn.onclick = function () {
+
+        friendRequestSocket.send(JSON.stringify({
+            'message': 'Friend Request',
+            'recipient': username.textContent,
+            'sender': window.chatConfig.currentUserUsername
+        }));
+        console.log('Friend Request Sent');
+    }
+}
