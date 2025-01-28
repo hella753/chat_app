44385040b5c8afdd75547efd0ad8593639916ba0
Kristine Gaprindashvili <kristigaphrindashvili@gmail.com>
2025-01-28 16:39:17 +0400

    added icon

--- chat_application/settings.py
+++ chat_application/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG') == 'True'
 
-ALLOWED_HOSTS = ["chat-app-zunf.onrender.com"]
+ALLOWED_HOSTS = ["chat-app-zunf.onrender.com", 'localhost', '127.0.0.1']
 
 CSRF_TRUSTED_ORIGINS = ["https://chat-app-zunf.onrender.com"]
 
