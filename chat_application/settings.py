8209a1d07ae4f1ebda8eb0ef23a1bfe33de90aa2
Kristine Gaprindashvili <kristigaphrindashvili@gmail.com>
2025-01-28 15:57:10 +0400

    minor changes

--- chat_application/settings.py
+++ chat_application/settings.py
@@ -28,13 +28,16 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG') == 'True'
 
-ALLOWED_HOSTS = ["chat-app-zunf.onrender.com"]
+ALLOWED_HOSTS = ["chat-app-zunf.onrender.com", "localhost", "127.0.0.1"]
 
 CSRF_TRUSTED_ORIGINS = ["https://chat-app-zunf.onrender.com"]
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
+
+if not os.path.exists('/tmp/media'):
+    os.makedirs('/tmp/media')
 
 # Application definition
 
